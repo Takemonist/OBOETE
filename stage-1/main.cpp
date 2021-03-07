164df92814f7d43e23bd9c0f5d6d828adedfaf99
Takemonist <gardenoftakemoto@gmail.com>
2021-03-08 01:03:47 +0900

    firstcommit

---
+++ stage-1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/* ↑はおまじない */
+
+int main(){
+    /*変数の宣言と初期化*/
+    
+    int a;                      /*変数の宣言*/
+
+    int b = 0;                  /*変数の初期化*/
+    int c = 10;                 /*初期化は0でなくてもよい*/
+
+    cout << c << endl;          /*変数cの表示*/
+
+    /*変数への代入*/
+
+    c = 5;                      /*左辺に右辺の値を代入する*/
+
+    cout << c << endl;
+
+    return 0;
+}
